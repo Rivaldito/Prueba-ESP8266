52fc0f69fc580e5b27df3a4394cbf3348e3775ea
Rivaldito <araquesebastian@gmail.com>
2019-04-20 11:00:41 -0400

    Segundo commit

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 require('./config')
-let express = require('express')
+const express = require('express')
+const bodyParser = require('body-parser')
 
 
-let app = express()
+const app = express()
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
 
 
 app.get('/', (req, res) => {
 
     res.json({
-        hola: 12
-    });
+        data
+    })
 
 });
+
+app.post('/data', (req, res) => {
+
+    let body = req.body;
+
+    var data = {
+        numero: body.numero
+    }
+
+    res.json(data)
+
+    console.log('HOLA')
+})
 
 app.listen(process.env.PORT)
